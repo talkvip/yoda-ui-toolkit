17827dd3b7fea1fbfca3bff81754d299352585be
simo <simo@SM-MACPRO.local>
2016-08-23 14:50:58 +0200

    sm

--- dist/__tests__/test-test.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-jest.mock('react-dom'); // To prevent issue of duplicate env. injection
-describe('actions', function () {
-    it('test', function () {
-        expect(1).toBe(1);
-    });
-});
-//# sourceMappingURL=test-test.js.map
--- dist/lib/Navbar.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-
-var react_bootstrap_1 = require('react-bootstrap');
-exports.Navbar = react_bootstrap_1.Navbar;
-//# sourceMappingURL=Navbar.js.map
