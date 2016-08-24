ad9a5fead6dce625b2c29c8ca2a191d63b9fd20e
simo <simo@SM-MACPRO.local>
2016-08-24 11:40:27 +0200

    sm

--- src/demo.tsx
+++ src/demo.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react';
 import {Button,Navbar} from './lib';
-<<<<<<< HEAD
 import TableDemoAjax from './demo/TableDemoAjax';
 import TableDemo from './demo/TableDemo';
 
@@ -40,19 +39,5 @@ class Menu extends React.Component<any,any> {
 }
 
 
-
-=======
-import TableDemo from './demo/TableDemo';
-
-const Menu = () =>(
-    <div>
-        <h1>Ui-kit</h1>
-        <Button> Hello</Button>
-        <TableDemo/>
-        <hr/>
-
-    </div>
-)
->>>>>>> ee3e863394b9b20685c5aba864a6f7903b73c79e
 
 export default Menu;
