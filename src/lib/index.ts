a60edc4f87cdf944fac16bb78ddfeb839d92af8c
Daniele Giove <daniele.giove@pioneerinvestments.com>
2016-09-02 09:36:48 +0200

    dg

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,8 +1,5 @@
 export * from './Spinner';
 export * from './Search';
 export * from './Grid';
-<<<<<<< HEAD
 export * from './TableProps';
-=======
 export * from './DatePicker';
->>>>>>> 207c506228a4630a7ab94b4ce3be52c5f0283147
