b40a283bd09aa31becb467af6897435ab1aa89fb
simo <simo@SM-MACPRO.local>
2016-09-13 15:34:21 +0200

    removed console log.

--- src/lib/Search/AutoComplete.tsx
+++ src/lib/Search/AutoComplete.tsx
@@ -170,7 +170,6 @@ export default class AutoComplete<T> extends React.Component<IProps<T>, IState<T
     }
 
     render() {
-        console.log('state',this.state);
         const values = this.props.selected ? [].concat(this.props.selected) : [];
         const items = (this.props.items || []).concat(values);
 
